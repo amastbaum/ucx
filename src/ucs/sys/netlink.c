f951865a1c16ea0b38ed48f02b09414ed2678527
Alma Mastbaum <amastbaum@nvidia.com>
2024-09-17 17:33:03 +0300

    UCT: added few more fixes

--- src/ucs/sys/netlink.c
+++ src/ucs/sys/netlink.c
@@ -26,7 +26,8 @@ ucs_status_t netlink_socket_create(struct netlink_socket *nl_sock, int protocol)
     nl_sock->fd = fd;
     nl_sock->local.nl_family = AF_NETLINK;
 
-    if (bind(fd, (struct sockaddr *)&nl_sock->local, sizeof(nl_sock->local)) < 0) {
+    if (bind(fd, (struct sockaddr *)&nl_sock->local,
+             sizeof(nl_sock->local)) < 0) {
         close(fd);
         ucs_diag("failed to bind netlink socket %d\n", fd);
         return UCS_ERR_IO_ERROR;
@@ -43,7 +44,8 @@ void netlink_socket_close(struct netlink_socket *nl_sock)
     }
 }
 
-void netlink_msg_init(struct netlink_message *msg, int type, int flags, int nlmsg_len)
+void netlink_msg_init(struct netlink_message *msg, int type,
+                      int flags, int nlmsg_len)
 {
     struct nlmsghdr *nlh;
 
@@ -56,7 +58,8 @@ void netlink_msg_init(struct netlink_message *msg, int type, int flags, int nlms
     nlh->nlmsg_pid = getpid();
 }
 
-ucs_status_t netlink_send(struct netlink_socket *nl_sock, struct netlink_message *msg)
+ucs_status_t netlink_send(struct netlink_socket *nl_sock,
+                          struct netlink_message *msg)
 {
     struct nlmsghdr *nlh = (struct nlmsghdr *)msg->buf;
 
@@ -87,14 +90,15 @@ int netlink_recv(struct netlink_socket *nl_sock, struct netlink_message *msg)
     return ret;
 }
 
-ucs_nl_parse_status_t netlink_parse_msg(struct netlink_message *msg,
-                                        int msg_len,
-                                        void (*parse_cb)(struct nlmsghdr *h, void *arg),
-                                        void *arg)
+ucs_nl_parse_status_t netlink_parse_msg(
+                            struct netlink_message *msg, int msg_len,
+                            void (*parse_cb)(struct nlmsghdr *h, void *arg),
+                            void *arg)
 {
     struct nlmsghdr *nlh;
 
-    for (nlh = (struct nlmsghdr *)msg->buf; NLMSG_OK(nlh, msg_len); nlh = NLMSG_NEXT(nlh, msg_len)) {
+    for (nlh = (struct nlmsghdr *)msg->buf; NLMSG_OK(nlh, msg_len);
+         nlh = NLMSG_NEXT(nlh, msg_len)) {
         if (nlh->nlmsg_type == NLMSG_DONE) {
             return UCS_NL_STATUS_DONE;
         }
--- src/ucs/sys/netlink.h
+++ src/ucs/sys/netlink.h
@@ -4,7 +4,6 @@
 #include <ucs/type/status.h>
 #include <sys/socket.h>
 #include <linux/netlink.h>
-#include <linux/rtnetlink.h>
 
 #define NETLINK_BUFFER_SIZE 8192
 
@@ -33,17 +32,14 @@ void netlink_msg_init(struct netlink_message *msg, int type,
                       int flags, int nlmsg_len);
 
 // Message Sending and Receiving
-ucs_status_t netlink_send(struct netlink_socket *nl_sock, struct netlink_message *msg);
+ucs_status_t netlink_send(struct netlink_socket *nl_sock,
+                          struct netlink_message *msg);
 int netlink_recv(struct netlink_socket *nl_sock, struct netlink_message *msg);
 
 // Message Parsing
-ucs_nl_parse_status_t netlink_parse_msg(struct netlink_message *msg,
-                                        int msg_len,
-                                        void (*callback)(struct nlmsghdr *h, void *arg),
-                                        void *arg);
-
-// Routing-specific Functions
-int netlink_get_routes(struct netlink_socket *nl_sock, int family);
-int netlink_route_is_reachable(const char *iface, struct sockaddr_storage *dest);
+ucs_nl_parse_status_t netlink_parse_msg(
+                            struct netlink_message *msg, int msg_len,
+                            void (*parse_cb)(struct nlmsghdr *h, void *arg),
+                            void *arg);
 
 #endif // UCS_NETLINK_H
