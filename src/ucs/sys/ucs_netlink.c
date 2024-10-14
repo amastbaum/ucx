ec744fbaa222be4e055c9b81f7235fd8c259843b
Alma Mastbaum <amastbaum@nvidia.com>
2024-10-14 12:16:52 +0300

    UCT: CR fixes

--- src/ucs/sys/ucs_netlink.c
+++ src/ucs/sys/ucs_netlink.c
@@ -49,49 +49,47 @@ ucs_netlink_send_cmd(int protocol, void *nl_protocol_hdr,
                      size_t nl_protocol_hdr_size, char *recv_msg_buf,
                      size_t *recv_msg_buf_len, unsigned short nlmsg_type)
 {
-    ucs_status_t ret;
+    ucs_status_t status;
     int fd;
     struct nlmsghdr nlh;
     struct iovec iov[2];
-    size_t send_msg_len = NLMSG_LENGTH(nl_protocol_hdr_size);
+    size_t dummy;
 
-    ret = ucs_netlink_socket_create(&fd, NETLINK_ROUTE);
-    if (ret != UCS_OK) {
+    status = ucs_netlink_socket_create(&fd, protocol);
+    if (status != UCS_OK) {
         ucs_diag("failed to open netlink socket");
-        return ret;
+        return status;
     }
 
-    memset(iov, 0, sizeof(iov));
     memset(&nlh, 0, sizeof(nlh));
-    nlh.nlmsg_len   = send_msg_len;
+    nlh.nlmsg_len   = NLMSG_LENGTH(nl_protocol_hdr_size);
     nlh.nlmsg_type  = nlmsg_type;
     nlh.nlmsg_flags = NLM_F_REQUEST | NLM_F_DUMP;
     nlh.nlmsg_seq   = 1;
-    nlh.nlmsg_pid   = getpid();
     iov[0].iov_base = &nlh;
     iov[0].iov_len  = sizeof(nlh);
     iov[1].iov_base = nl_protocol_hdr;
     iov[1].iov_len  = nl_protocol_hdr_size;
 
     do {
-        ret = ucs_socket_sendv_nb(fd, iov, 2, &send_msg_len);
-    } while (ret == UCS_ERR_NO_PROGRESS);
+        status = ucs_socket_sendv_nb(fd, iov, 2, &dummy);
+    } while (status == UCS_ERR_NO_PROGRESS);
 
-    if (ret != UCS_OK) {
-        ucs_diag("failed to send netlink message. returned %d", ret);
+    if (status != UCS_OK) {
+        ucs_diag("failed to send netlink message. returned %d", status);
         goto out;
     }
 
     do {
-        ret = ucs_socket_recv_nb(fd, recv_msg_buf, recv_msg_buf_len);
-    } while (ret == UCS_ERR_NO_PROGRESS);
+        status = ucs_socket_recv_nb(fd, recv_msg_buf, recv_msg_buf_len);
+    } while (status == UCS_ERR_NO_PROGRESS);
 
-    if (ret != UCS_OK) {
+    if (status != UCS_OK) {
         ucs_diag("failed to receive route netlink message");
         goto out;
     }
 
 out:
     ucs_close_fd(&fd);
-    return ret;
+    return status;
 }
